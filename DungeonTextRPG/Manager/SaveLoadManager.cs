bbe20aa0aceedd65cfccc2a50b24d9ecdda4f0bf
dalsi <tkdwns8632@gmail.com>
2025-01-30 03:54:36 +0900

    feat(SaveLoadManager): player클래스 값 로드하는 기능 추가
    - 저장된 JSON파일에서 Player데이터를 로드해서 GameManager의 MyPlayer에 값 할당.

--- DungeonTextRPG/Manager/SaveLoadManager.cs
+++ DungeonTextRPG/Manager/SaveLoadManager.cs
@@ -45,23 +45,38 @@ namespace DungeonTextRPG.Manager.SaveLoad
 
         public bool LoadData() // 저장파일 로드
         {
-            bool hasSaveFile = File.Exists("saveData.json");
+            string filePath = "saveData.json";
 
-            if (hasSaveFile)
+            if (File.Exists(filePath))
             {
                 Console.WriteLine("저장 파일이 존재합니다.");
+                ApplyLoadedData(filePath);
+
+                Console.WriteLine("게임을 시작하려면 아무 키나 누르세요...");
+                Console.ReadKey(); // 콘솔 종료 방지
+
+                Console.Clear();
+
+                return true;
             }
             else
             {
                 Console.WriteLine("저장 파일이 존재하지 않습니다.");
+
+                Console.WriteLine("게임을 시작하려면 아무 키나 누르세요...");
+                Console.ReadKey(); // 콘솔 종료 방지
+
+                Console.Clear();
+
+                return false;
             }
+        }
 
-            Console.WriteLine("게임을 시작하려면 아무 키나 누르세요...");
-            Console.ReadKey(); // 콘솔 종료 방지
-
-            Console.Clear();
-
-            return hasSaveFile;
+        void ApplyLoadedData(string filePath)
+        {
+            // JSON 데이터를 Player 객체로 변환
+            string jsonData = File.ReadAllText(filePath);
+            GameManager.instance.MyPlayer = JsonConvert.DeserializeObject<Player>(jsonData);
         }
     }
 }
