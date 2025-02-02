72cc490d7050f7bfe475b816fd03fc84432166de
dalsi <tkdwns8632@gmail.com>
2025-02-02 15:15:16 +0900

    feat(SaveLoadManager): 상점 매진 현황 저장&불러오기 기능 추가
    ItemDatabase.instance.Items를 저장 및 불러오기

--- DungeonTextRPG/Manager/SaveLoadManager.cs
+++ DungeonTextRPG/Manager/SaveLoadManager.cs
@@ -2,6 +2,7 @@
 using DungeonTextRPG.Manager.Inventory;
 using Newtonsoft.Json;
 using System.Text;
+using System.Text.Json;
 
 namespace DungeonTextRPG.Manager.SaveLoad
 {
@@ -17,6 +18,7 @@ namespace DungeonTextRPG.Manager.SaveLoad
         string filePath_Player = "saveData_Player.json";
         string filePath_Inventory = "saveData_Inventory.json";
         string filePath_Equip = "saveData_Equip.json";
+        string filePath_Shop = "saveData_Shop.json";
 
         public void SaveData()
         {
@@ -28,22 +30,25 @@ namespace DungeonTextRPG.Manager.SaveLoad
             File.WriteAllText(filePath_Player, json_Player, Encoding.UTF8);
 
 
-
             /// 인벤토리 저장
             var settings = new JsonSerializerSettings
             {
-                TypeNameHandling = TypeNameHandling.Auto
-            };
-
+                TypeNameHandling = TypeNameHandling.Auto,
+                Formatting = Formatting.Indented,
+            }; 
             string json_Inventory = JsonConvert.SerializeObject(InventoryManager.instance.MyInventory, settings);
             File.WriteAllText(filePath_Inventory, json_Inventory, Encoding.UTF8);
-
 
 
             /// 장착아이템 저장
             string json_Equip = JsonConvert.SerializeObject(InventoryManager.instance.SlotStatus, settings);
             File.WriteAllText(filePath_Equip, json_Equip, Encoding.UTF8);
 
+
+            /// 상점 진열 아이템 매진 현황 저장
+            string json_Shop = JsonConvert.SerializeObject(ItemDatabase.instance.Items, settings);
+            File.WriteAllText(filePath_Shop, json_Shop, Encoding.UTF8);
+            
 
             Console.WriteLine("게임 데이터를 저장했습니다.");
         }
@@ -96,6 +101,11 @@ namespace DungeonTextRPG.Manager.SaveLoad
             /// Equip
             string jsonData_Equip = File.ReadAllText(filePath_Equip);
             InventoryManager.instance.SlotStatus = JsonConvert.DeserializeObject<Dictionary<string, EquipmentItem>>(jsonData_Equip, settings);
+
+
+            /// Shop
+            string jsonData_Shop = File.ReadAllText(filePath_Shop);
+            ItemDatabase.instance.Items = JsonConvert.DeserializeObject<Dictionary<string, EquipmentItem>>(jsonData_Shop, settings);
         }
     }
 }
