8ad48128de0dc279931126302f421a1a159974ae
dalsi <tkdwns8632@gmail.com>
2025-02-03 09:23:51 +0900

    feat(CSVReader): csvreader 추가
    CSV 사용에 앞서 기능 추가

---
+++ DungeonTextRPG/CsvReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class CsvReader
+{
+    private string _filePath;
+
+    public CsvReader(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+    public List<string[]> ReadCSV()
+    {
+        List<string[]> data = new List<string[]>();
+
+        using (StreamReader reader = new StreamReader(_filePath))
+        {
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                string[] values = line.Split(','); // 쉼표(,) 기준으로 분리
+                data.Add(values);
+            }
+        }
+        return data;
+    }
+}
